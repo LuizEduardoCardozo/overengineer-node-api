94480617f77929dcf4ddaeac9aac6599d29c9501
LuizEduardoCardozo <eduard.cardoz@gmail.com>
2020-10-04 17:06:45 -0300

    change: database name extension from sqlite3 to sqlite

--- knexfile.ts
+++ knexfile.ts
@@ -3,7 +3,7 @@ import path from 'path';
 module.exports = ({
 	client: 'sqlite3',
 	connection: {
-		filename: path.resolve(__dirname, 'src', 'database', 'dbs', 'database_1.sqlite3')
+		filename: path.resolve(__dirname, 'src', 'database', 'dbs', 'database_1.sqlite')
 	},
 	migrations: {
 		directory: path.resolve(__dirname, 'src', 'tools', 'migrations')
