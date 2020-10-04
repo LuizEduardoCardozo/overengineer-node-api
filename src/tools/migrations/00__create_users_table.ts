352865faaeeff2d0811eff02a8aeb4e14590372b
LuizEduardoCardozo <eduard.cardoz@gmail.com>
2020-10-04 17:08:14 -0300

    add: user role to database

--- src/tools/migrations/00__create_users_table.ts
+++ src/tools/migrations/00__create_users_table.ts
@@ -6,6 +6,7 @@ export async function up( knex:Knex ): Promise<void> {
 		table.string('username').notNullable();
 		table.string('email').notNullable().unique();
 		table.string('password').notNullable();
+		table.integer('role').defaultTo(0);
 		table.timestamp('created_at').defaultTo('now()');
 	});
 }
